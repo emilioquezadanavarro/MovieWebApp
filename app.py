25518f5a6500a8e07e399803daa0b77f5c575620
Emilio Quezada <equezadanavarro@gmail.com>
2025-11-26 12:03:53 +0100

    index, add_user & user_movies routes implemented

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, url_for
 from data_manager import DataManager
 from models import db, User, Movie
 import os
+from dotenv import load_dotenv
+
+# Load the environment variables from .env file
+load_dotenv()
+
+# Constants for API
+API_KEY = os.environ.get('OMDB_API_KEY')
+API_URL = "http://omdbapi.com/"
 
 # Setting up flask app
 app = Flask(__name__)
@@ -10,6 +18,7 @@ app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data/movies.db')}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'a_very_secret_key_for_flashing_messages'
 
 # Link the database and the app. This is the reason you need to import db from models
 db.init_app(app)
@@ -19,17 +28,47 @@ data_manager = DataManager()
 
 # Routes
 @app.route('/')
-def home():
+def index():
+
     users = data_manager.get_users()
     return render_template('index.html', users=users)
 
 @app.route('/users', methods=['POST'])
 def add_user():
-    pass
+
+    # Retrieve the input name
+    username = request.form.get('username')
+
+    if username:
+
+        data_manager.create_user(username)
+
+        # Success message using flash function
+        flash(f"The user '{username}' was successfully added")
+
+        # Redirect the user to the GET Route
+        return redirect(url_for('index'))
+
+    else:
+        # Name field is missing - display error
+        flash('Field is mandatory.', 'error')
+        return redirect(url_for('index'))
+
 
 @app.route('/users/<int:user_id>/movies', methods=['GET'])
-def user_movies():
-    pass
+def user_movies(user_id):
+
+    user, movies = data_manager.get_movies(user_id)
+
+    if user is None:
+
+        # Error message - display error
+        flash(f"User not found!" , "error")
+
+        # Redirect the user to the GET Route
+        return redirect(url_for('index'))
+
+    return render_template('movies.html',user=user ,movies=movies)
 
 @app.route('/users/<int:user_id>/movies', methods=['POST'])
 def add_movie_to_user():
